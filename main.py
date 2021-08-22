872f1c1f59f16119f97c760b6b623dff93f1e8ce
Roberto Robles <rob2thinking@gmail.com>
2021-08-22 14:58:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Imports           #
 #####################
 
+from re import I
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
@@ -18,7 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
 
-db_fields = {
+resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
     'views': fields.Integer,
@@ -26,94 +27,64 @@ db_fields = {
 }
 
 #####################
-# Arguments         #
+# Request Parser    #
 #####################
 
-insert_args = reqparse.RequestParser()
-insert_args.add_argument("name", type=str, help="Name of the video", required=True)
-insert_args.add_argument("views", type=int, help="Views of the video", required=True)
-insert_args.add_argument("likes", type=int, help="Likes on the video", required=True)
-
-update_args = reqparse.RequestParser()
-update_args.add_argument("name", type=str, help="Name of the video is required")
-update_args.add_argument("views", type=int, help="Views of the video")
-update_args.add_argument("likes", type=int, help="Likes on the video")
+video_put_args = reqparse.RequestParser()
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Views of the video", required=True)
+video_put_args.add_argument("likes", type=int, help="Likes on the video", required=True)
 
 #####################
 # Classes           #
 #####################
 
 class VideoModel(db.Model):
-  
+
     id     = db.Column(db.Integer, primary_key=True)
-    
     name   = db.Column(db.String(100), nullable=False)
-    
     views  = db.Column(db.Integer, nullable=False)
-    
     likes  = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return f"Video(name = {name}, views = {views}, likes = {likes})"
 
+# only need to run once and then comment out, 
+# or it will override the data in the database
+#
+# db.create_all() 
+
 class Video(Resource):
-  
-    @marshal_with(db_fields)
+
+    @marshal_with(resource_fields)
     def get(self, video_id):
-      
+
         result = VideoModel.query.filter_by(id=video_id).first()
-        
+
         if not result:
-          
-            abort(404, message="Could not find video with that id")
-            
+
+            abort(404, message="Could  not find video with that id")
+
         return result
 
-    @marshal_with(db_fields)
+    @marshal_with(resource_fields)
     def put(self, video_id):
-      
-        args = insert_args.parse_args()
-        
-        result = VideoModel.query.filter_by(id=video_id).first()
-        
+
+        args    = video_put_args.parse_args()
+
+        result  = VideoModel.query.filter_by(id=video_id).first()
+
         if result:
-          
-            abort(409, message="Video id taken...")
+
+            abort(409, message="Video id taken")
 
         video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
-        
+
         db.session.add(video)
-        
+
         db.session.commit()
-        
+
         return video, 201
-
-    @marshal_with(db_fields)
-    def patch(self, video_id):
-      
-        args = update_args.parse_args()
-        
-        result = VideoModel.query.filter_by(id=video_id).first()
-        
-        if not result:
-          
-            abort(404, message="Video doesn't exist, cannot update")
-
-        if args['name']:
-          
-            result.name = args['name']
-
-        if args['views']:
-          
-            result.views = args['views']
-
-        if args['likes']:
-          
-            result.likes = args['likes']
-
-        db.session.commit()
-
-        return result
 
 #####################
 # Main              #
@@ -126,5 +97,5 @@ api.add_resource(Video, "/video/<int:video_id>")
 #####################
 
 if __name__ == "__main__":
-  
+    
     app.run(debug=True)
